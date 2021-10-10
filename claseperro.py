cccee9f3a0c6781c6aa11692dd938850b74936be
AleFuentes982 <jano231982@gmail.com>
2021-10-09 23:37:04 -0300

    first commit

---
+++ claseperro.py
@@ -0,0 +1,14 @@
+class Perro:
+
+   def ladrar(self):
+     print("ladrar")
+
+
+sparky = Perro()
+print ("sparky ya puede ") 
+sparky.ladrar()
+
+
+fluffy = Perro()
+print ("fluffy ya puede ") 
+fluffy.ladrar()
